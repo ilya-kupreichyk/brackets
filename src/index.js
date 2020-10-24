08e128e9e8a964a9c29499e235002e65c0c60df5
ilya-kupreichyk <kupreichykilya@gmail.com>
2020-10-24 12:07:51 +0300

    feat: create functions: brackets to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let stack = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        stack.push(bracketsConfig[i].join(''))
+    }
+
+    for (let i = 0; i < stack.length; i++) {
+        if (str.replace(stack[i], '').length !== str.length) {
+            str = str.replace(stack[i], '');
+            i = -1;
+        }
+    }
+
+    return str.length == 0 ? true : false;
 }
